700244a228916ff7a7f883c3dd0765de77284aee
Мустафа <magometovmustafa@gmail.com>
2022-12-19 22:36:20 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import AddTodos from './components/users/AddTodos';
+import EditUsers from './components/EditTodos/EditUsers';
+import UsersList from './components/users/UsersList';
+import ContextComp from './ContextComp';
+
+function App() {
+  return (
+    <div>
+      <ContextComp>
+        <BrowserRouter>
+          <Routes>
+            <Route path='add-todos' element={<AddTodos />} />
+            <Route path='todos' element={<UsersList />} />
+            <Route path='edit-user/:id' element={<EditUsers />} />
+          </Routes>
+        </BrowserRouter>
+      </ContextComp>
+
+    </div>
+  );
+}
+
+export default App;
